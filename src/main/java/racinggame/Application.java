924f6629180fc5e4ecca72df13f656e9e148934f
kim-sunhwa <shpring.bases@gmail.com>
2021-10-13 03:01:55 +0900

    initial commit

--- src/main/java/racinggame/Application.java
+++ src/main/java/racinggame/Application.java
@@ -1,7 +1,12 @@
 package racinggame;
 
+import nextstep.utils.Console;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO 자동차 경주 게임 구현
-    }
+	public static void main(String[] args) {
+		// TODO 자동차 경주 게임 구현
+		System.out.println("시작");
+		String s = Console.readLine();
+		System.out.println(s);
+	}
 }

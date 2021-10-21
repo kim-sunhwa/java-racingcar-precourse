2352633db27c4c7e65d848e28653ac4de94096cb
kim-sunhwa <shpring.bases@gmail.com>
2021-10-21 19:44:44 +0900

    refactor (RacingInfo) : 반복문을 재귀로 변경

--- src/main/java/racinggame/controller/RacingInfo.java
+++ src/main/java/racinggame/controller/RacingInfo.java
@@ -4,6 +4,7 @@ import static racinggame.commons.response.ErrorCode.*;
 
 import nextstep.utils.Console;
 import racinggame.commons.exceptions.BaseException;
+import racinggame.commons.exceptions.InvalidTypeException;
 import racinggame.commons.response.Message;
 import racinggame.commons.validations.Validator;
 import racinggame.model.Cycle;
@@ -16,42 +17,31 @@ public class RacingInfo {
 	}
 
 	public Cycle ready() {
-		boolean isValid = false;
-		int number = -1;
-		while (!isValid) {
-			number = isValidTypeNumber();
-			isValid = isValidRangeOfNumber(new Cycle(number));
-		}
-		return new Cycle(number);
-	}
-
-	private boolean isValidRangeOfNumber(Cycle cycle) {
 		try {
-			validator.of(cycle);
-			return true;
+			String response = requestCycle();
+			isNumber(response);
+			int cycle = Integer.parseInt(response);
+			isValidRange(cycle);
+			return new Cycle(cycle);
 		} catch (BaseException exception) {
 			Message.send(exception.getMessage());
-			return false;
+			return ready();
 		}
 	}
 
-	private int isValidTypeNumber() {
-		boolean isValid = false;
-		String cycleStr = "";
-		while (!isValid) {
-			Message.requestCycle();
-			cycleStr = Console.readLine();
-
-			isValid = isNumber(cycleStr);
-		}
-		return Integer.parseInt(cycleStr);
+	private String requestCycle() {
+		Message.requestCycle();
+		return Console.readLine();
 	}
 
-	private boolean isNumber(String cycleStr) {
-		boolean isValid = Validator.isNumber(cycleStr);
-		if (!isValid) {
-			Message.send(INVALID_TYPE_OF_NUMBER);
+	private void isNumber(String cycleStr) {
+		if (!Validator.isNumber(cycleStr)) {
+			throw new InvalidTypeException(INVALID_TYPE_OF_NUMBER);
 		}
-		return isValid;
 	}
+
+	private void isValidRange(int number) {
+		validator.of(new Cycle(number));
+	}
+
 }
